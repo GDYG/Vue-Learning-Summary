ebb0032863fad19c0160c6fe9e75081c16d82e80
GDYG <35097370+GDYG@users.noreply.github.com>
2019-08-21 21:51:36 +0800

    Add files via upload

---
+++ 18-webpack中的config分离/vue/Cpn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>我是组件标题</h2>
+    <p>{{name}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Cpn",
+    data() {
+      return {
+        name: '我是组件内容'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
