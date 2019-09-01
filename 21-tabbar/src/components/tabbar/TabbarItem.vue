2c65d2b35c8b1392c266e79b97295b2e80034c01
GDYG <35097370+GDYG@users.noreply.github.com>
2019-09-01 17:53:00 +0800

    Add files via upload

---
+++ 21-tabbar/src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="tab-bar-item" @click="btnClick">
+    <div :style="isActive"><slot name="item-icon"></slot></div>
+    <div :style="isActive"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabbarItem",
+    computed: {
+      imgColor() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      isActive() {
+        return this.imgColor ? {color: this.itemColor} : {}
+       }
+    },
+    props: {
+      path: String,
+      itemColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    methods: {
+      btnClick() {
+        this.$router.push(this.path);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+
+    vertical-align: middle;
+    margin-top: 7px;
+  }
+  .item-font{
+    font-size: 14px;
+  }
+  span{
+    font-size: 20px;
+  }
+</style>
